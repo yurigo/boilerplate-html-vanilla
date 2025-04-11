9922e4d70ebe29f42b501f061ce610ca8b15a474
La Salle BCN <lspublic@salle.salleURL.edu>
2025-04-11 12:46:56 +0200

    chore: 🔔 animation
    - dingdong
    - dongding
    
    bonus: header

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("Hello world!");
+
+const campana = document.getElementById("campana");
+const header = document.getElementById("cabecera");
+
+console.log(campana);
+
+const handleClick = (e) => {
+  e.stopPropagation();
+  //   console.log("hc");
+  //   header.style.display = "block";
+  header.classList.remove("invisible");
+};
+const handleClickDocument = (e) => {
+  console.log("hcd");
+  //   header.style.display = "none";
+  header.classList.add("invisible");
+};
+
+campana.addEventListener("click", handleClick);
+
+document.addEventListener("click", handleClickDocument);
